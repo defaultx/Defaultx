a867d77358ec200d0fd3cd4af736adf0d8ba2f5c
Defaultx <rahulthomas91@outlook.com>
2016-04-16 18:14:52 +0100

    Added code to check if the phone has nfc capabilities and if the nfc is turned on.

--- src/com/example/Defaultx/MainPage.java
+++ src/com/example/Defaultx/MainPage.java
@@ -1,25 +1,39 @@
 package com.example.Defaultx;
 
 import android.app.Activity;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.androidhive.loginandregister.R;
 
 /**
  * Created by rahul on 06/11/2015.
  */
 public class MainPage extends Activity {
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        NfcAdapter nfcAdpt;
         // Set View to main.xml
         setContentView(R.layout.main);
         TextView macAddressField = (TextView) findViewById(R.id.macAdressField);
         //macAddressField.append(mac);
         macAddressField.setText(getIntent().getStringExtra("KEY_StringMacAddress"));
+
+        nfcAdpt = NfcAdapter.getDefaultAdapter(this);
+        // Check if the smartphone has NFC
+        if (nfcAdpt == null) {
+            Toast.makeText(this, "NFC not supported", Toast.LENGTH_LONG).show();
+            finish();
+        }
+        // Check if NFC is enabled
+        if (!nfcAdpt.isEnabled()) {
+            Toast.makeText(this, "Enable NFC before using the app", Toast.LENGTH_LONG).show();
+        }
 
         Button btnCheckOut = (Button) findViewById(R.id.btnCheckOut);
         // Listening to Login Screen link
@@ -31,6 +45,12 @@ public class MainPage extends Activity {
                 finish();
             }
         });
+
+       /* MimeRecord mimeRecord = new MimeRecord();
+        mimeRecord.setMimeType("text/plain");
+        mimeRecord.setData("This is my data".getBytes("UTF-8"));*/
+
+
     }
 
 }
